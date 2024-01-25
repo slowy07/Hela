da2f5b59ae4ce38d6f605b77bf0d0290c288e712
slowy07 <slowy.arfy@gmail.com>
2024-01-25 10:17:46 +0700

    menambahkan .gitignore

--- Hela/math.py
+++ Hela/math.py
@@ -1,5 +1,3 @@
-angka: int = 10
-
 def tambah(a: int, b: int) -> int:
     return a + b
 
@@ -14,3 +12,24 @@ def faktorial(angka: int) -> int:
     for i in range(1, angka + 1):
         hasil *= i
     return hasil
+
+def jumlah_deret_geometri(utama: int, rasio_umum: int, jumlah: int) -> int:
+    if rasio_umum == 1:
+        return jumlah * utama
+    return (utama / (1 - rasio_umum)) * (1 - rasio_umum**jumlah)
+
+def modus(arr: list[int | float]) -> int | float:
+    count = []
+    for value in arr:
+        count.append(arr.count(value))
+    combine = dict(zip(arr, count))
+    memo = [a for a, b in combine.items() if b == max(count)]
+    min_value = memo[0]
+    result = None
+    for value in memo:
+        if value < min_value:
+            min_value = value
+    result = min_value
+    if result == 1:
+        raise ValueError("nan")
+    return result
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import Hela.math as math
 
 print(math.tambah(15, 2))
 print(math.faktorial(6))
+print(math.jumlah_deret_geometri(1, 2, 10))
