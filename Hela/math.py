26074885d7b78f0ac5f49885a46594f06bce785c
slowy07 <slowy.arfy@gmail.com>
2024-01-29 12:00:35 +0700

    menambahkan custom error

--- Hela/math.py
+++ Hela/math.py
@@ -1,6 +1,5 @@
 import math
 import Hela.util.error as error
-from typing import Union
 
 def tambah(a: int, b: int) -> int:
     """
@@ -9,7 +8,7 @@ def tambah(a: int, b: int) -> int:
     parameter:
         a (int): angka pertama
         b (int): angka kedua
-
+    
     return:
         (int): hasil dari angka pertama + angka kedua
     """
@@ -18,7 +17,6 @@ def tambah(a: int, b: int) -> int:
     else:
         return error.TipeError(["int"])
 
-
 def kurang(a: int, b: int) -> int:
     """
     membuat fungsi pengurangan
@@ -26,7 +24,7 @@ def kurang(a: int, b: int) -> int:
     parameter:
         a (int): angka pertama
         b (int): angka kedua
-
+    
     return:
         (int): hasil dari angka pertama - angka kedua
     """
@@ -35,7 +33,6 @@ def kurang(a: int, b: int) -> int:
     else:
         return error.TipeError(["int"])
 
-
 def bagi(a: int, b: int) -> int:
     """
     membuat fungsi pembagian
@@ -43,11 +40,11 @@ def bagi(a: int, b: int) -> int:
     parameter:
         a (int): angka pertama
         b (int): angka kedua
-
+    
     return:
         (int): hasil dari angka pertama / angka kedua
     """
-    if isinstance(a, int) and isinstance(b, int):
+    if isinstance(a, int) and isinstance(b, int): 
         try:
             return a / b
         except ZeroDivisionError:
@@ -55,24 +52,22 @@ def bagi(a: int, b: int) -> int:
     else:
         return error.TipeError(["int"])
 
-
 def faktorial(angka: int) -> int:
     """
     menghitung faktorial dari sebuah input nilai
-
+    
     parameter:
         angka (int): angka yang ingin dimasukkan
     return:
         int: angka yang dihasilkan daripada proses faktorial
     """
-    if isinstance(angka, int):
+    if isinstance(angka, int): 
         hasil = 1
         for i in range(1, angka + 1):
             hasil *= i
         return hasil
     else:
         return error.TipeError(["int"])
-
 
 def jumlah_deret_geometri(utama: int, rasio_umum: int, jumlah: int) -> int:
     """
@@ -86,17 +81,12 @@ def jumlah_deret_geometri(utama: int, rasio_umum: int, jumlah: int) -> int:
     return:
         (int): hasil deret geometri dengan rumus Sn = a(1-r^n)/(1-r)
     """
-    if (
-        isinstance(utama, int)
-        and isinstance(rasio_umum, int)
-        and isinstance(jumlah, int)
-    ):
+    if isinstance(utama, int) and isinstance(rasio_umum, int) and isinstance(jumlah, int):
         if rasio_umum == 1:
             return jumlah * utama
         return (utama / (1 - rasio_umum)) * (1 - rasio_umum**jumlah)
     else:
         return error.TipeError(["int"])
-
 
 def modus(arr: list[int | float]) -> int | float:
     """
@@ -106,7 +96,7 @@ def modus(arr: list[int | float]) -> int | float:
     parameter:
         arr (list[int atau float]): parameter data
 
-
+    
     return:
         (int atau float): output ini muncul sebagai sesuai dengan item yang sering terjadi
     """
@@ -126,8 +116,7 @@ def modus(arr: list[int | float]) -> int | float:
             raise ValueError("nan")
         return result
     else:
-        return error.TipeError(["int"])
-
+        return error.TipeError(["int", "float"])
 
 def normal_pdf(x: float, mean: float, sigma: float) -> float:
     """
@@ -148,9 +137,3 @@ def normal_pdf(x: float, mean: float, sigma: float) -> float:
         return pdf_value
     else:
         return error.TipeError(["int"])
-
-
-def luas_lingkaran(jari: Union[float, int]) -> float:
-    if not isinstance(jari, (float, int)):
-        return error.TipeError(["float", "int"])
-    return math.pi * jari**2
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import Hela.math as math
 
-# print(math.tambah("1", 3))
-print(math.bagi(8, 0))
+print(math.tambah("1", 3))
+# print(math.bagi(8, 0))
