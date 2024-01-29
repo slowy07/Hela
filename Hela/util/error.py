fc0be1fad497b963a6f251f7ce546c078abdc067
slowy07 <slowy.arfy@gmail.com>
2024-01-29 11:48:58 +0700

    ..

--- Hela/util/error.py
+++
@@ -1,9 +0,0 @@
-class TipeError(TypeError):
-    def __init__(self, tipe_data: list):
-        pesan = f"Error: kamu memasukkan tipe data yang salah, seharusnya adalah {' atau '.join(tipe_data)}"
-        super().__init__(pesan)
-
-class ErrorDibagiNol(ZeroDivisionError):
-    def __init__(self):
-        pesan = f"Error: Tidak dibagikan dengan nol!"
-        super().__init__(pesan)
