d4fce25eafc5f3ac82b98a190f9fa4ad77fdc58a
slowy07 <slowy.arfy@gmail.com>
2024-02-02 11:45:28 +0700

    Revert "menambahkan dokumentasi"
    This reverts commit d3c75713fb15e7cb94bc5fe7c1bb9bf911104691.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Hela",
-    version="1.0.0",
-    description="package untuk memudahkan untuk berhitung",
+    name = "hela",
+    version = "1.0.0",
+    description="package untuk memudahkan perhitungan",
     author="arfy slowy",
+    author_email="slowy.arfy@contoh.mail.com",
     package_data={"Hela": ["util/*"]},
-    python_requires=">=3.10",
     packages=find_packages()
 )
